efcc9d17a39379e59ddf41674a7018a4c79b468a
thurstanngo <146325171+thurstan-ngo@users.noreply.github.com>
2026-03-05 23:30:11 -0500

    Implement LRU eviction policy

--- main.py
+++ main.py
@@ -14,8 +14,24 @@ def fifo(k, requests):
     return miss_count
 
 
+def lru(k, requests):
+    cache = []
+    miss_count = 0
+    for r in requests:
+        if r not in cache:
+            miss_count += 1
+            if len(cache) < k:
+                cache.append(r)
+            else:
+                del cache[0]
+                cache.append(r)
+        else:
+            cache.remove(r)
+            cache.append(r)
+    return miss_count
+
+
 def main():
-    
     input_file = sys.argv[1]
 
     # Read input file
@@ -29,7 +45,9 @@ def main():
         str_requests = file.readline().split()
         requests = [int(r) for r in str_requests]
 
-    print(f'FIFO: {fifo(k, requests)}')
+    # Run cache eviction policies on requests list
+    print(f'FIFO  : {fifo(k, requests)}')
+    print(f'LRU   : {lru(k, requests)}')
 
 
 if __name__ == '__main__':
